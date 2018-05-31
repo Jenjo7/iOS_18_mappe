fb13a8c770f4722350c9fadedb95c44556fba4a3
Gianni Savini <gianni.savini@studio.unibo.it>
2018-05-31 16:38:22 +0200

    Creato repo dell'esercizio delle mappe

--- iOS_18_mappe/ViewController.swift
+++ iOS_18_mappe/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
